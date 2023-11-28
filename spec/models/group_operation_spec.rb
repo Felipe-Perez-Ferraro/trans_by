57453855f5e0a43687ee7e9876e82f844b19126a
Felipe <fperezferraro@gmail.com>
2023-11-28 17:38:35 -0300

    Created model specs to group_operation

--- spec/models/group_operation_spec.rb
+++ spec/models/group_operation_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe GroupOperation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.create(name: 'Felipe', email: 'example@mail.com', password: 'password',
+                        password_confirmation: 'password')
+    @group = Group.create(name: 'Transaction', icon: 'URL', user_id: @user)
+    @operation = Operation.create(name: 'Transaction', amount: 1000, author_id: @user)
+    GroupOperation.create(group_id: @group, operation_id: @operation)
+  end
+  
+  describe 'Foreign keys' do
+    it { should belong_to(:group) }
+    it { should belong_to(:operation) }
+  end
 end
